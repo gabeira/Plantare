03e52be2dbc015855939d1a733170d8c79d947b3
Gabriel Bernardo Pereira <gabeira@gmail.com>
2018-02-26 23:30:38 +1100

    Update menu options, Implement Google Login, Set login required to use the app

--- app/src/main/java/mobi/plantare/MainActivity.kt
+++ app/src/main/java/mobi/plantare/MainActivity.kt
@@ -11,20 +11,22 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.facebook.CallbackManager
 import com.facebook.FacebookSdk
 import com.facebook.share.widget.ShareDialog
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
-import mobi.plantare.fragments.ContributorsFragment
-import mobi.plantare.fragments.GardenMapFragment
-import mobi.plantare.fragments.SocialFragment
-import mobi.plantare.fragments.UserFragment
+import mobi.plantare.fragments.*
+import mobi.plantare.view.utility.GlideApp
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {//}, FacebookCallback<Sharer.Result> {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, UserFragment.OnUserLoginListener {
 
     private var callbackManager: CallbackManager? = null
     private var shareDialog: ShareDialog? = null
+    private var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,10 +46,43 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         nav_view.setNavigationItemSelectedListener(this)
 
-        //Initialize first fragment, maybe use Garden
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.content_main, SocialFragment())
-                .commit()
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth?.currentUser
+        if (currentUser != null) {
+            updateUserInfo(currentUser.displayName!!, currentUser.email!!, currentUser.photoUrl.toString())
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.content_main, SocialFragment.newInstance())
+                    .commit()
+        } else {
+            updateUserInfo("", "", "")
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.content_main, UserFragment.newInstance())
+                    .commit()
+        }
+
+        plante.setOnClickListener({
+            val intent = Intent(applicationContext, PlantActivity::class.java)
+//            intent.putExtra(GardenMapFragment.LOCATION_TO_PLANT, myLocationToPlant)
+            //TODO Implement add plant without location
+            startActivityForResult(intent, GardenMapFragment.REQUEST_PLANT)
+        })
+    }
+
+    override fun updateUserInfo(name: String, email: String, photoUrl: String) {
+        val headerView = nav_view.getHeaderView(0)
+        val userPhoto = headerView.findViewById<ImageView>(R.id.user_photo)
+        val userName = headerView.findViewById<TextView>(R.id.user_name)
+        val userEmail = headerView.findViewById<TextView>(R.id.user_email)
+        userName?.text = name
+        userEmail?.text = email
+
+        GlideApp.with(applicationContext)
+                .load(photoUrl)
+                .centerInside()
+                .circleCrop()
+                .into(userPhoto)
+
     }
 
 //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
@@ -151,31 +186,58 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         val id = item.itemId
 
-        if (id == R.id.nav_plantation) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.content_main, GardenMapFragment())
-                    .commit()
+        if (id == R.id.nav_plants) {
+            val currentUser = auth?.currentUser
+            if (currentUser != null) {
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.content_main, SocialFragment.newInstance())
+                        .commit()
+            } else {
+                updateUserInfo("", "", "")
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.content_main, UserFragment.newInstance())
+                        .commit()
+            }
 
-        } else if (id == R.id.nav_social) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.content_main, SocialFragment.newInstance())
-                    .commit()
+        } else if (id == R.id.nav_map) {
+            val currentUser = auth?.currentUser
+            if (currentUser != null) {
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.content_main, GardenMapFragment.newInstance())
+                        .commit()
+            } else {
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.content_main, UserFragment.newInstance())
+                        .commit()
+            }
 
-        } else if (id == R.id.nav_user) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.content_main, UserFragment())
-                    .commit()
+        } else if (id == R.id.nav_my_plants) {
+            val currentUser = auth?.currentUser
+            if (currentUser != null) {
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.content_main, MyPlantsFragment.newInstance())
+                        .commit()
+            } else {
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.content_main, UserFragment.newInstance())
+                        .commit()
+            }
+
         } else if (id == R.id.nav_share) {
             startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(this@MainActivity)
                     .setType("text/plain")
                     .setText(getString(R.string.play_store_app_url))
                     .intent, getString(R.string.action_share)))
+
         } else if (id == R.id.nav_contributors) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.content_main, ContributorsFragment.newInstance())
                     .commit()
-        } else if (id == R.id.nav_info) {
 
+        } else if (id == R.id.nav_settings) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.content_main, UserFragment.newInstance())
+                    .commit()
         }
 
         val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
